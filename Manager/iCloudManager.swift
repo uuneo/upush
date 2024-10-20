865dd55bbeab03cea861e25ff4238d5c2185454f
UUNEO <uuneo@qq.com>
2024-10-20 11:08:39 +0800

    ceshi icloud

--- Manager/iCloudManager.swift
+++ Manager/iCloudManager.swift
@@ -20,8 +20,19 @@ class iCloudManager{
 		debugPrint(containerURL)
 		
 		do{
-			let fileURL = containerURL.appendingPathComponent("upush").appendingPathComponent("hello.txt")
+			
+			let fileURL = containerURL.appendingPathComponent("Documents").appendingPathComponent("hello.txt")
 			try "hello world".write(to: fileURL, atomically: true, encoding: .utf8)
+			
+			if FileManager.default.fileExists(atPath: fileURL.path) {
+				// 文件成功保存到iCloud
+				debugPrint("保存成功",fileURL)
+			} else {
+				// 文件保存到iCloud失败
+				debugPrint("保存失败",fileURL)
+			}
+			
+			
 		}catch{
 			print(error.localizedDescription)
 		}
